a13db4f327a4f9827154c933d80ce6ba7ff33db6
Pritam7Chakraborty <cpritam870@gmail.com>
2025-11-07 21:34:14 +0530

    trying to deploy - 1

--- server/tournamate-backend/server.js
+++ server/tournamate-backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 
 // ---------- CONFIG ----------
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Allowed frontend origins
